4cf205cb299b342ec4d8f849c3e3f16d9fa9550f
antoinehage <me@antoinehage.com>
2020-07-22 12:19:44 -0400

    added ui seed with login/logout

--- ui/soajsio/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ ui/soajsio/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+import {UracService} from '../../services/urac.service';
+import {AuthenticationService} from '../../services/authentication.service';
+
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
@@ -7,8 +10,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SidenavListComponent implements OnInit {
   @Output() sidenavClose = new EventEmitter();
+  userInfo = null;
 
-  constructor() { }
+  constructor(
+    private uracService: UracService,
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit() {
   }
@@ -17,4 +24,16 @@ export class SidenavListComponent implements OnInit {
     this.sidenavClose.emit();
   }
 
+  login() {
+    this.authenticationService.login("owner", "password");
+    setTimeout(() => {
+      this.userInfo = this.uracService.getUser();
+      console.log(this.userInfo);
+    }, 1000);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.userInfo = null;
+  }
 }
